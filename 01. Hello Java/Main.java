2822465fdf1400aa94d331db68d4623daa606d74
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-04-01 02:13:03 +0530

    Hello Java

---
+++ 01. Hello Java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // THIS IS MY FIRST JAVA PROGRAM
+
+        /**
+         * THIS IS 
+         * A MULTILINE 
+         * COMMENT
+         */
+        
+        System.out.println("I like Pizza!");
+        System.out.println("It's really good!");
+        System.out.println("Buy me pizza!");
+
+    }
+
+}
